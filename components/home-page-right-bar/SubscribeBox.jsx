21cb96fe2416e0a29dde41aab5a67c805e527cb7
Gökhan Demir <gkndmr96@gmail.com>
2023-09-27 21:19:14 +0300

    Update SubscribeBox.jsx

--- components/home-page-right-bar/SubscribeBox.jsx
+++ components/home-page-right-bar/SubscribeBox.jsx
@@ -4,7 +4,7 @@ import style from './style.module.css'
 function SubscribeBox() {
   return (
     <section className={style.subscribeBox}>
-      <p>Premium'a Abone Ol</p>
+      <p>Premium&apos;a Abone Ol</p>
       <p>Yeni özellikleri açmak için abone ol ve uygun olman durumunda reklam geliri payı kazan.</p>
       <Link href={'/'}>Abone ol</Link>
     </section>
